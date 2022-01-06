32477478f3d8064addeb4738d97911c2c844bc14
fahimahammed <fahimahammed.cse@gmail.com>
2022-01-07 01:50:45 +0600

    solve

---
+++ output-3.cpp
@@ -0,0 +1,14 @@
+///https://www.beecrowd.com.br/judge/en/problems/view/2749
+#include<iostream>
+
+int main()
+{
+    printf("---------------------------------------\n");
+	printf("|x = 35                               |\n");
+	printf("|                                     |\n");
+	printf("|               x = 35                |\n");
+	printf("|                                     |\n");
+	printf("|                               x = 35|\n");
+	printf("---------------------------------------\n");
+	return 0;
+}
