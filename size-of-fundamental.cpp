5785bdcc4797aafbb99c942bb3cf96011f0ed71a
fahimahammed <fahimahammed.cse@gmail.com>
2022-01-06 21:53:28 +0600

    size of fundamental

---
+++ size-of-fundamental.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Size of char : " << sizeof(char) << "\n";
+    cout << "Size of int : " << sizeof(int) << "\n";
+    cout << "Size of float : " << sizeof(float) << "\n";
+    cout << "Size of double : " << sizeof(double) << "\n";
+    cout << "Size of long : " << sizeof(long) << "\n";
+    cout << "Size of long long : " << sizeof(long long) << "\n";
+    cout << "Size of short : " << sizeof(short) << "\n";
+    cout << "Size of long double : " << sizeof(long double) << "\n";
+    return 0;
+}
