06b16ba4a88cdbb1dab1c4c014027e79604fccfe
fahimahammed <fahimahammed.cse@gmail.com>
2022-01-07 02:07:03 +0600

    solve

---
+++ output-9.cpp
@@ -0,0 +1,12 @@
+///https://www.beecrowd.com.br/judge/en/problems/view/2755
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"\"Ro'b'er\tto\\/\"\n";
+    cout<<"(._.) ( l: ) ( .-. ) ( :l ) (._.)\n";
+    cout<<"(^_-) (-_-) (-_^)\n";
+    cout<<"(\"_\") ('.')\n";
+    return 0;
+}
