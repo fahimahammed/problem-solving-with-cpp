ae3d2de976aabfd5be5abcfe0f6eda2bc8afcd79
fahimahammed <fahimahammed.cse@gmail.com>
2022-01-07 01:46:24 +0600

    solve

---
+++ output-2.cpp
@@ -0,0 +1,14 @@
+///https://www.beecrowd.com.br/judge/en/problems/view/2748
+#include<iostream>
+
+int main()
+{
+    printf("---------------------------------------\n");
+    printf("|        Roberto                      |\n");
+    printf("|                                     |\n");
+    printf("|        5786                         |\n");
+    printf("|                                     |\n");
+    printf("|        UNIFEI                       |\n");
+    printf("---------------------------------------\n");
+    return 0;
+}
