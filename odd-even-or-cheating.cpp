a2f685bacb7c31c9fc865dcccc178c804b619fd9
fahimahammed <fahimahammed.cse@gmail.com>
2022-01-06 23:09:14 +0600

    even odd or cheating

--- odd-even-or-cheating.cpp
+++ odd-even-or-cheating.cpp
@@ -10,10 +10,13 @@ int main()
     cin>>r;
     cin>>a;
     int sum=j1+j2;
-    if((sum%2==0 && p==1) || (sum%2==1 && p==0)) win = 1;
+    if((sum%2==0 && p==1) || (sum%2==1 && p==0))
+        win = 1;
     else win = 2;
-    if((r==1 && a==0) || (r==0 && a==1)) win = 1;
-    else if(r==1 && a==1) win=2;
+    if((r==1 && a==0) || (r==0 && a==1))
+        win = 1;
+    else if(r==1 && a==1)
+        win=2;
     cout <<"Jogador "<<win<<" ganha!\n";
     return 0;
 }
