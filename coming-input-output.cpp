1bf28b443f57201605370e8357017364a1364e24
fahimahammed <fahimahammed.cse@gmail.com>
2022-01-07 20:55:10 +0600

    coming input and output solve

--- coming-input-output.cpp
+++ coming-input-output.cpp
@@ -1,1 +1,18 @@
+///https://www.beecrowd.com.br/judge/en/problems/view/2765
+#include<iostream>
+#include<cstring>
+using namespace std;
 
+int main()
+{
+    string text;
+    getline(cin, text);
+    for(int i=0; i<text.length(); i++){
+        if(text[i] == ','){
+            cout<<endl;
+        }
+        else cout<<text[i];
+    }
+    cout<<endl;
+    return 0;
+}
